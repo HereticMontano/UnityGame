2a6273ee212ed5dbc0dbb9afdb593279737b53c8
Montano <manuelelp@gmail.com>
2020-05-09 23:08:41 +0200

    primer commit

---
+++ Assets/Scripts/HelperCalculate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public static class HelperCalculate
+    {
+        public static float CalculateJumpForce(float gravityStrength, float jumpHeight)
+        {
+            //h = v^2/2g
+            //2gh = v^2
+            //sqrt(2gh) = v
+            return Mathf.Sqrt(2 * gravityStrength * jumpHeight);
+        }
+    }
+}
---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,101 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+
+    public float speed = 0.4f;
+    public float jumpForce;
+    public float jumpTime;
+    
+    private Rigidbody2D playerRb;
+    private bool isGrounded = true;
+    private float direction;
+    private float jumpCounter;
+    private bool isJumping;
+
+    private bool isMovingRight;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        direction = Input.GetAxis("Horizontal");
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            isMovingRight = false;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            isMovingRight = true;
+        }
+
+        Jump();
+        ManageAnimation();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D col) => isGrounded = false;
+
+    private void Jump()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            isJumping = true;
+            jumpCounter = jumpTime;
+            playerRb.velocity = Vector2.up * jumpForce;
+        }
+       
+        if(Input.GetKey(KeyCode.UpArrow) && isJumping)
+        {
+            if (jumpCounter > 0)
+            {
+                playerRb.velocity = Vector2.up * jumpForce;
+                jumpCounter -= Time.deltaTime;
+                Debug.Log(jumpCounter);
+            }
+            else
+                isJumping = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            isJumping = false;
+        }
+
+    }
+    private void Move()
+    {
+        playerRb.velocity = new Vector2(speed * direction, playerRb.velocity.y);
+    }
+
+    private void ManageAnimation()
+    {
+        bool isMoving = direction != 0;
+        if (isMoving)
+        {
+            if (isMovingRight)
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            else
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            
+            GetComponent<Animator>().SetFloat("DirectionX", 1);
+        }
+        
+        GetComponent<Animator>().SetBool("Moving", isMoving);
+    }
+}
