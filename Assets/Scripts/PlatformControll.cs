5bad036810bda40b4e3809faa9ea27e8bcbb6401
Montano <manuelelp@gmail.com>
2020-05-13 16:00:20 +0200

    Mecanicas de colisiones (con respecto a las plataformas y pies del personaje) mas refinadas

---
+++ Assets/Scripts/PlatformControll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlatformControll : MonoBehaviour
+{
+
+    public float waitingTime = 0.5f;
+    private float counter;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (counter <= 0)
+            {
+                GetComponent<PlatformEffector2D>().useColliderMask = true;
+                counter = waitingTime;
+            }
+            else
+                counter -= Time.deltaTime; 
+        }
+        else if(!Input.GetKey(KeyCode.DownArrow))
+            GetComponent<PlatformEffector2D>().useColliderMask = false;
+    }
+}
+
