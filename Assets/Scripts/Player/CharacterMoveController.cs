20b70814549a9496f0ba0c1d3737a0d946240ed6
Montano <manuelelp@gmail.com>
2020-05-24 14:47:24 +0200

    Commit

---
+++ Assets/Scripts/Player/CharacterMoveController.cs
@@ -0,0 +1,70 @@
+﻿using Assets.Scripts.Player;
+using UnityEngine;
+
+public class CharacterMoveController : BaseMoveController
+{
+    public float lowJumpMultiplier = 2.5f;
+
+    private float movement;
+    private bool isMovingRight;
+
+    private FootController foot;
+
+    private void Start()
+    {
+        foot = GetComponentInChildren<FootController>();
+    }
+   
+    private void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            isMovingRight = false;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            isMovingRight = true;
+        }
+
+        foot.Jump(jumpForce);
+        Fall();
+        ManageAnimation();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Fall()
+    {
+        if (!BaseFall() && rb.velocity.y > 0 && !Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    protected override void Move()
+    {
+        if (foot.HitWall)
+            rb.velocity = new Vector2(speed * movement, rb.velocity.y);
+    }
+
+    protected override void ManageAnimation()
+    {
+        bool isMoving = movement != 0;
+        if (isMoving)
+        {
+            if (isMovingRight)
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            else
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+
+            GetComponent<Animator>().SetFloat("DirectionX", 1);
+        }
+
+        GetComponent<Animator>().SetBool("Moving", isMoving);
+    }
+}
---
+++ Assets/Scripts/Player/FootController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class FootController : MonoBehaviour
+    {
+        public LayerMask groundLayer;
+        public Rigidbody2D mainBody;
+
+        private BoxCollider2D box;
+        private int cantJumps;
+        private void Awake()
+        {
+            box = GetComponent<BoxCollider2D>();
+        }
+
+        public bool IsGround
+        {
+            get
+            {
+                //Debug.DrawRay(box.bounds.max, Vector2.down * 21, Color.red, 1);
+                //Debug.DrawRay(box.bounds.min, Vector2.down * 21, Color.green, 1);
+
+                RaycastHit2D hitRight = Physics2D.Raycast(box.bounds.max, Vector2.down, 2, groundLayer);
+                RaycastHit2D hitLeft = Physics2D.Raycast(box.bounds.min, Vector2.down, 2, groundLayer);
+                if (hitRight.collider != null || hitLeft.collider != null)
+                {
+                    cantJumps = 2;
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        public bool HitWall
+        {
+            get
+            {
+                //Debug.DrawRay(box.bounds.min, Vector2.left * 21, Color.green, 1);
+                //Debug.DrawRay(box.bounds.max, Vector2.right * 21, Color.red, 1);
+
+                RaycastHit2D hitRight = Physics2D.Raycast(box.bounds.max, Vector2.right, 2, groundLayer);
+                RaycastHit2D hitLeft = Physics2D.Raycast(box.bounds.min, Vector2.left, 2, groundLayer);
+                return hitRight.collider == null && hitLeft.collider == null;
+            }
+        }
+
+        public void Jump(float jumpForce)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow) && (IsGround || cantJumps > 0))
+            {
+                if (cantJumps > 0)
+                {
+                    cantJumps--;
+                    mainBody.velocity = Vector2.up * jumpForce;
+                }
+            }
+        }
+    }
+}
