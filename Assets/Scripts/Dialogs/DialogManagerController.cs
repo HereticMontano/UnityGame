0de372f9ca28de3fcdc6ba964ad290916459510e
Montano <manuelelp@gmail.com>
2020-05-26 11:20:39 +0200

    commit

---
+++ Assets/Scripts/Dialogs/DialogManagerController.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogManagerController : MonoBehaviour
+{
+    Queue<string> sentences;
+
+    public Canvas board;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StarDialog(Dialog dialogue)
+    {
+        board.GetComponentInChildren<TextMeshProUGUI>().text = dialogue.sentences[0];
+        
+        board.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 0);
+    }
+}
---
+++ Assets/Scripts/Dialogs/DialogTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class DialogTrigger: MonoBehaviour
+    {
+        public Dialog dialogue;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            FindObjectOfType<DialogManagerController>().StarDialog(dialogue);
+            Destroy(gameObject);
+        }
+        
+    }
+}
